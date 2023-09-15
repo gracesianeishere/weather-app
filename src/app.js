eb5c5a82fdb830e3aec33245c822251a66864189
gracesianeishere <143945096+itsgracesiane@users.noreply.github.com>
2023-09-16 00:42:00 +0700

    Moved HTML block to JS

--- src/app.js
+++ src/app.js
@@ -13,6 +13,33 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  days.forEach(function(day) {
+    forecastHTML = forecastHTML +
+      `
+      <div class="col-2">
+        <div class="weather-forecast-date">
+        ${day}
+        </div>
+        <img src="https://openweathermap.org/img/wn/01n@2x.png" width="42"/>
+        <div class="weather-forecast-temperature">
+        <span class="weather-forecast-temperature-max">
+        33° </span>
+        <span class="weather-forecast-temperature-min">
+        24° </span>
+        </div>
+        </div>
+  `;
+  })
+  
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
     let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
@@ -75,3 +102,4 @@ let celciusLink = document.querySelector("#celcius-link");
 celciusLink.addEventListener("click", displayCelciusTemperature);
 
 search("Jakarta");
+displayForecast();
